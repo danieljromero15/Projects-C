0308198b8e29fca4732959e039538b22cd766fa6
Daniel Romero <danieljromero305@gmail.com>
2023-12-05 16:31:40 -0500

    Updated project to work with Windows

--- Projects/Numbers/PITotheNth.c
+++ Projects/Numbers/PITotheNth.c
@@ -5,35 +5,35 @@ int multiplyIntArray(int *ArrayToMultiply, int sizeOfArray) {
     int product = 1;
 
     if (n == 0) {
-        //printf("Array is empty!");
+        // printf("Array is empty!");
         product = 0;
     } else {
         for (int i = 0; i < n; i++) {
             product *= (*ArrayToMultiply + i);
-            //printf("%d ", (*ArrayToMultiply + i));
+            // printf("%d ", (*ArrayToMultiply + i));
         }
     }
-    //printf("\n\nThe product of the array is %d", product);
+    // printf("\n\nThe product of the array is %d", product);
 
     return product;
 }
 
-int arrayShift(int *ArrayToShift, int sizeOfArray){
-    for(int i=0; i < sizeOfArray / 4; i++){
+int arrayShift(int *ArrayToShift, int sizeOfArray) {
+    for (int i = 0; i < sizeOfArray / 4; i++) {
         ArrayToShift[i] += 2;
     }
 }
 
 double nilakantha(int accuracy) {
     double PICalc = 3;
-    int array[] = {2,3,4};
+    int array[] = {2, 3, 4};
 
-    for(int i = 0; i < accuracy; i++){
-        switch(i % 2){
-            case 0: // even
+    for (int i = 0; i < accuracy; i++) {
+        switch (i % 2) {
+            case 0:  // even
                 PICalc += (4.0 / multiplyIntArray(array, sizeof(array)));
                 break;
-            case 1: // odd
+            case 1:  // odd
                 PICalc -= (4.0 / multiplyIntArray(array, sizeof(array)));
                 break;
             default:
@@ -41,12 +41,12 @@ double nilakantha(int accuracy) {
                 exit(22);
                 break;
         }
-        //printf("Before: %d %d %d", array[0], array[1], array[2]);
+        // printf("Before: %d %d %d", array[0], array[1], array[2]);
         arrayShift(array, sizeof(array));
-        //printf("\tAfter: %d %d %d\n", array[0], array[1], array[2]);
+        // printf("\tAfter: %d %d %d\n", array[0], array[1], array[2]);
     }
 
-    //const double PI = 3;
+    // const double PI = 3;
 
     return PICalc;
 }
@@ -54,16 +54,18 @@ double nilakantha(int accuracy) {
 int main() {  // How the fuck did I mess up the int main so much i literally
               // know this shit wtf
     // im so tired
+    //printf("\e[1;1H\e[2J"); // clears screen
+    printf("\n");
 
     int inputNum;
 
     // scanf("Number of digits (max x): %d", &inputNum);
 
     // printf("\n%d\n\n", sizeof(array));
-    //multiplyIntArray(array, sizeof(array));
+    // multiplyIntArray(array, sizeof(array));
 
-    printf("%.20f", nilakantha(100000000));
-    
+    printf("%.20f", nilakantha(10000));
+
     printf("\n\n");
     return 0;
 }
