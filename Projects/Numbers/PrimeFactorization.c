c7f3030219e3b54a33a9affa849cbead33c14aa3
danieljromero15 <danieljromero305@gmail.com>
2023-12-06 16:54:05 -0500

    worked on PrimeFactorization

--- Projects/Numbers/PrimeFactorization.c
+++ Projects/Numbers/PrimeFactorization.c
@@ -6,14 +6,37 @@ void printIntArray(int *arrayToPrint, int sizeOfArray){
     }
 }
 
+struct factorsStruct{
+    int factorsArrayLen;
+    int factorsArray[255];
+};
+
+
+struct factorsStruct primeFactorization(int startNum){
+    struct factorsStruct s1;
+
+    s1.factorsArrayLen = 0;
+    for(int i = 1; i < startNum; ++i){
+        if(startNum % i == 0){
+            s1.factorsArray[s1.factorsArrayLen] = i;
+            s1.factorsArrayLen++;
+        }
+    }
+
+    printf("%d", s1.factorsArray[0]);
+
+
+    return s1;
+}
+
 int main(){
     int numberToFind;
 
     printf("Please enter the number for which to find the Prime Factors: ");
     scanf("%d", &numberToFind);
 
-    int array[] = {1, 2, 3, 4, 5};
-    printIntArray(array, sizeof(array));
+    printIntArray(primeFactorization(numberToFind).factorsArray, primeFactorization(numberToFind).factorsArrayLen);
 
+    printf("\n");
     return 0;
 }
