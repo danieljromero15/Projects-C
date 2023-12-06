d8c0e429443ecff3e20be102a4cb04c212b188d1
danieljromero15 <danieljromero305@gmail.com>
2023-12-05 19:33:59 -0500

    Finished PITotheNth to the best of my ability

--- Projects/Numbers/PITotheNth.c
+++ Projects/Numbers/PITotheNth.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define _USE_MATH_DEFINES
 
-unsigned long multiplyIntArray(int *ArrayToMultiply, int sizeOfArray) {
+long multiplyIntArray(int *ArrayToMultiply, int sizeOfArray) {
     int n = sizeOfArray / 4;
-    unsigned long product = 1;
+    long product = 1;
 
     if (n == 0) {
         // printf("Array is empty!");
@@ -13,7 +16,7 @@ unsigned long multiplyIntArray(int *ArrayToMultiply, int sizeOfArray) {
             // printf("%d ", (*ArrayToMultiply + i));
         }
     }
-    printf("\nThe product of the array is %lu\n", product);
+    //printf("\nThe product of the array is %lu\n", product);
 
     return product;
 }
@@ -41,7 +44,7 @@ double nilakantha(int accuracy) {
                 exit(22);
                 break;
         }
-        printf("Before: %d %d %d\t%d", array[0], array[1], array[2], i);
+        //printf("Before: %d %d %d\t%d", array[0], array[1], array[2], i);
         arrayShift(array, sizeof(array));
         // printf("\tAfter: %d %d %d\n", array[0], array[1], array[2]);
     }
@@ -59,15 +62,19 @@ int main() {  // How the fuck did I mess up the int main so much i literally
 
     int inputNum;
 
-    // scanf("Number of digits (max x): %d", &inputNum);
+    printf("Number of digits (max 11): ");
+
+    scanf("%d", &inputNum);
 
     // printf("\n%d\n\n", sizeof(array));
     // multiplyIntArray(array, sizeof(array));
+    double pi = nilakantha(1000000);
+    
+    printf("\n\nMy calculation of PI is %.*f",inputNum, pi);
+    printf("\nPI is \t\t\t%.*f", inputNum, M_PI);
+    // TODO set max to 1000000
 
-    printf("%.20f", nilakantha(813));
-
-    int array[] = {1626, 1627, 1628};
-    printf("\n\n%lu\n\n", multiplyIntArray(array, sizeof(array)));
+    //printf("\n\n%lu\n\n", multiplyIntArray(array, sizeof(array)));
 
     printf("\n\n");
     return 0;
