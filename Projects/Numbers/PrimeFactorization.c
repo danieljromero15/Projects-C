a8e194e95f81606b6cd340dc3e7a606de654ee20
danieljromero15 <danieljromero305@gmail.com>
2023-12-06 15:50:44 -0500

    began PrimeFactorization

---
+++ Projects/Numbers/PrimeFactorization.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void printIntArray(int *arrayToPrint, int sizeOfArray){
+    for(int i = 0; i < sizeOfArray; ++i){
+        printf("%d ", (*arrayToPrint + i));
+    }
+}
+
+int main(){
+    int numberToFind;
+
+    printf("Please enter the number for which to find the Prime Factors: ");
+    scanf("%d", numberToFind);
+
+    int array[] = {1, 2, 3, 4};
+    printIntArray(array, sizeof(array));
+
+    return 0;
+}
