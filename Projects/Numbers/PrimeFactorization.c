1a5d7e38f13efd0f2db446ccbca83344dce39103
Daniel Romero <danieljromero305@gmail.com>
2023-12-07 03:58:06 -0500

    worked on PrimeFactorization

--- Projects/Numbers/PrimeFactorization.c
+++ Projects/Numbers/PrimeFactorization.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct integerArray {
+    int *intArray;
+    int intArraySize;
+};
+
+int *resizeAndClearArray(int size) { return (int *)malloc(sizeof(int) * size); }
+
 bool isPrime(int startNum) {  // just calculates if the number is prime
     bool isPrimeBool = true;
     if (startNum > 1) {
@@ -16,25 +23,24 @@ bool isPrime(int startNum) {  // just calculates if the number is prime
         exit(0);
     }
 
-    printf("\n");
+    // printf("\n");
     return isPrimeBool;
 }
 
-void printIntArray(int *arrayToPrint,
-                   int sizeOfArray) {  // prints integer array
-    for (int i = 0; i < sizeOfArray / 4; ++i) {
-        printf("%d ", (*arrayToPrint + i));
+// prints integer array
+void printIntArray(int *arrayToPrint, int sizeOfArray) {
+    for (int i = 0; i < sizeOfArray; i++) {
+        printf("%d ", *(arrayToPrint + i));
     }
 }
 
 long multiplyIntArray(int *ArrayToMultiply, int sizeOfArray) {
-    int n = sizeOfArray / 4;
     long product = 1;
 
-    if (n == 0) {
+    if (sizeOfArray == 0) {
         product = 0;
     } else {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < sizeOfArray; i++) {
             product *= (*ArrayToMultiply + i);
         }
     }
@@ -42,29 +48,81 @@ long multiplyIntArray(int *ArrayToMultiply, int sizeOfArray) {
     return product;
 }
 
-int primeArray(int StartNum){ // calculates all prime numbers before the startNum
-    
-}
-
-struct factorsStruct {
-    int factorsArrayLen;
-    int factorsArray[255];
-};
-
-struct factorsStruct primeFactorization(int startNum) {
-    struct factorsStruct s1;
-
-    s1.factorsArrayLen = 0;
-    for (int i = 1; i < startNum; ++i) {
-        if (startNum % i == 0) {
-            s1.factorsArray[s1.factorsArrayLen] = i;
-            s1.factorsArrayLen++;
+// calculates all prime numbers before the startNum
+struct integerArray primeNumsBefore(int startNum) {
+    struct integerArray primeArray;
+    primeArray.intArray = resizeAndClearArray(startNum);
+    primeArray.intArraySize = 0;
+    for (int i = 2; i < startNum; ++i) {
+        // printf("%d", i);
+        if (isPrime(i)) {
+            // printf("is prime, adding to index %d", n);
+            primeArray.intArray[primeArray.intArraySize] = i;
+            // printf("\t%d\n", primeArray[n]);
+            ++primeArray.intArraySize;
+        } else {
+            // printf("is not prime\n");
         }
     }
+    // printIntArray(primeArray.intArray, primeArray.intArraySize);
+    // printf("\n");
+    return primeArray;
+}
 
-    printf("%d", s1.factorsArray[0]);
+struct integerArray factorization(int n) {
+    struct integerArray factors;
+    
+    factors.intArray = resizeAndClearArray(n);
+    factors.intArraySize = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            factors.intArray[factors.intArraySize] = i;
+            ++factors.intArraySize;
+            //factors.intArray = resizeAndClearArray(factors.intArraySize);
+            
+        }
+    }
+    return factors;
+}
 
-    return s1;
+struct integerArray primeFactorization(int startNum) {
+    struct integerArray primeFactors;
+
+    int *factorArray = factorization(startNum).intArray;
+    int factorArraySize = factorization(startNum).intArraySize;
+
+    printf("%d", factorArraySize);
+
+    primeFactors.intArray = resizeAndClearArray(startNum * 8);
+
+    primeFactors.intArraySize = 0;
+    primeFactors.intArray[0] = 0;
+    /*if (!isPrime(startNum)) { // this entire thing needs to be changed to its own recursive function
+        //while (multiplyIntArray(primeFactors.intArray, primeFactors.intArraySize) != startNum) {
+        while (primeFactors.intArraySize < 10) {
+            //printf("product: %d\n", multiplyIntArray(primeFactors.intArray, primeFactors.intArraySize));
+            primeFactors.intArray[primeFactors.intArraySize] = factorArray[1];
+
+            if(!isPrime(factorArray[factorArraySize - 1])){
+
+            }
+
+
+            ++primeFactors.intArraySize;
+
+            //printIntArray(primeFactors.intArray, primeFactors.intArraySize);
+        }
+    } else {
+        primeFactors.intArray[primeFactors.intArraySize] = startNum;
+        printf("\nYour number is already prime!");
+        exit(0);
+    }*/
+
+    printf("\nfinal: ");
+    printIntArray(primeFactors.intArray, primeFactors.intArraySize);
+    printf("\n");
+
+    return primeFactors;
 }
 
 int main() {
@@ -73,8 +131,19 @@ int main() {
     printf("Please enter the number for which to find the Prime Factors: ");
     scanf("%d", &numberToFind);
 
-    printIntArray(primeFactorization(numberToFind).factorsArray,
-                  primeFactorization(numberToFind).factorsArrayLen);
+    // printf("%d", primeNumsBefore(numberToFind));
+    // struct integerArray primeNumArray = primeNumsBefore(numberToFind);
+    // printIntArray(primeNumArray.intArray, primeNumArray.intArraySize);
+
+    printf("\n");
+
+    struct integerArray factorsArray = factorization(numberToFind);
+    printIntArray(factorsArray.intArray, factorsArray.intArraySize);
+
+    printf("\n");
+
+    struct integerArray primeFactorsArray = primeFactorization(numberToFind);
+    printIntArray(primeFactorsArray.intArray, primeFactorsArray.intArraySize);
 
     printf("\n");
     return 0;
