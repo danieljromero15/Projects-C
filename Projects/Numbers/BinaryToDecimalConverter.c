87c3b0087c902b81c25d2a962c793ad0112aa9c4
Daniel Romero <danieljromero305@gmail.com>
2024-02-06 20:16:10 -0500

    added statement about negatives

--- Projects/Numbers/BinaryToDecimalConverter.c
+++ Projects/Numbers/BinaryToDecimalConverter.c
@@ -13,11 +13,11 @@ void printIntArray(const int *arrayToPrint, int sizeOfArray) {
     }
 }
 
-int *reverseIntArray(const int *intArrayToReverse, int intArrayToReverseLength){ //will return pointer
+int *reverseIntArray(const int *intArrayToReverse, int intArrayToReverseLength) { //will return pointer
     int reversedArray[intArrayToReverseLength];
     int intArrayToReverseLengthTemp = intArrayToReverseLength;
 
-    for(int i=0; i < intArrayToReverseLength; i++){
+    for (int i = 0; i < intArrayToReverseLength; i++) {
         reversedArray[i] = intArrayToReverse[--intArrayToReverseLengthTemp];
     }
 
@@ -26,18 +26,18 @@ int *reverseIntArray(const int *intArrayToReverse, int intArrayToReverseLength){
     return &reversedArray[0];
 }
 
-unsigned long long longPow(int base, int power){
+unsigned long long longPow(int base, int power) {
     unsigned long long final = base;
-    for(int i=1; i < power; i++){
+    for (int i = 1; i < power; i++) {
         final *= base;
     }
     return final;
 }
 
-unsigned long long arrayToInt(const int *arrayToConvert, int arrayToConvertSize){ // works up to 1048575
+unsigned long long arrayToInt(const int *arrayToConvert, int arrayToConvertSize) { // works up to 1048575
     unsigned long long out = 0;
     unsigned long long numberOfZeroes = longPow(10, arrayToConvertSize - 1);
-    for(int i=0; i < arrayToConvertSize; i++){
+    for (int i = 0; i < arrayToConvertSize; i++) {
         out += arrayToConvert[i] * numberOfZeroes;
         numberOfZeroes /= 10;
     }
@@ -47,49 +47,41 @@ unsigned long long arrayToInt(const int *arrayToConvert, int arrayToConvertSize)
 
 unsigned long long convertToBinary(int decimalNumber) {
     int result = decimalNumber;
-    int resultArrLength=0;
+    int resultArrLength = 0;
     int resultArrLengthTemp;
 
-    if (decimalNumber > 0) {
-        //define array size
-        while (result > 0){
-            result /= 2;
-            resultArrLength++;
-        }
-        int resultArr[resultArrLength];
-
-        result = decimalNumber;
-        resultArrLengthTemp = resultArrLength - 1;
-        while (result > 0) {
-            //printf("%d ", result % 2);
-            resultArr[resultArrLengthTemp] = result % 2;
-            result /= 2;
-            resultArrLengthTemp--;
-        }
-
-
-        //printf("\nfinal:\narr\t");
-        //printIntArray(resultArr, resultArrLength);
-
-        //printf("\n");
-
-        //printIntArray(reverseIntArray(resultArr, resultArrLength), resultArrLength);
-        //printf("int\t%llu", arrayToInt(resultArr, resultArrLength));
-
-        return arrayToInt(resultArr, resultArrLength);
-
-    } else if (decimalNumber < 0) {
-        printf("\nnegative numbers not implemented yet\n");
-        return 0;
-    } else {
-        return 0;
+    //define array size
+    while (result > 0) {
+        result /= 2;
+        resultArrLength++;
     }
+    int resultArr[resultArrLength];
+
+    result = decimalNumber;
+    resultArrLengthTemp = resultArrLength - 1;
+    while (result > 0) {
+        //printf("%d ", result % 2);
+        resultArr[resultArrLengthTemp] = result % 2;
+        result /= 2;
+        resultArrLengthTemp--;
+    }
+
+
+    //printf("\nfinal:\narr\t");
+    //printIntArray(resultArr, resultArrLength);
+
+    //printf("\n");
+
+    //printIntArray(reverseIntArray(resultArr, resultArrLength), resultArrLength);
+    //printf("int\t%llu", arrayToInt(resultArr, resultArrLength));
+
+    return arrayToInt(resultArr, resultArrLength);
 }
 
 int main() {
     int numberToConvert = 0;
-    printf("Please input a number to convert to binary (up to 1048575): ");
-    scanf("%d", &numberToConvert);
+    printf("Please input a positive number to convert to binary (up to 1048575): ");
+    scanf("%u", &numberToConvert);
 
     printf("\nBinary: %llu", convertToBinary(numberToConvert));
 
