b202f570c19b20c164d106e51ed9cda0fff313c3
Daniel Romero <danieljromero305@gmail.com>
2024-02-08 14:07:44 -0500

    Worked on BinaryConverter.cpp

--- Projects/Numbers/BinaryConverter.cpp
+++ Projects/Numbers/BinaryConverter.cpp
@@ -1,3 +1,18 @@
 //
 // Created by danie on 2/8/2024.
 //
+#include <iostream>
+
+int* toBinary(int decimalNumberInput){
+    int binaryArray[] = {1, 2, 3,4};
+    return &binaryArray[0];
+}
+
+int main(){
+    int *binaryArray = toBinary(2);
+    //for(int i = 0; i < &binaryArray)
+    std::cout << sizeof(binaryArray); // this doesn't work, make an object
+    std::cout << "\n";
+    std::cout << toBinary(2);
+    return 0;
+}
