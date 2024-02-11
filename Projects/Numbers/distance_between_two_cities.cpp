fb59322d110850a43245616783400803c1195caf
danieljromero15 <danieljromero305@gmail.com>
2024-02-11 15:54:08 -0500

    Began distance_between_two_cities.cpp

---
+++ Projects/Numbers/distance_between_two_cities.cpp
@@ -0,0 +1,10 @@
+//
+// Created by daniel on 2/11/24.
+// Description: Calculates the distance between two cities and allows the user to specify a unit of distance.
+//              This program may require finding coordinates for the cities like latitude and longitude.
+//
+
+int main(){
+
+    return 0;
+}
