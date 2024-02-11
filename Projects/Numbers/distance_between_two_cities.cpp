11800c02ccd3f0f45eb7e28566b0c6c170e03cc5
danieljromero15 <danieljromero305@gmail.com>
2024-02-11 17:10:20 -0500

    Worked on distance_between_two_cities.cpp

--- Projects/Numbers/distance_between_two_cities.cpp
+++ Projects/Numbers/distance_between_two_cities.cpp
@@ -4,7 +4,45 @@
 //              This program may require finding coordinates for the cities like latitude and longitude.
 //
 
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+std::unordered_map<std::string, std::string, double, double> readWorldCities(const std::string& filePath){
+    std::unordered_map<std::string, std::string, double, double> cities;
+
+    std::fstream file(filePath);
+    std::string line;
+
+    if(file.is_open()){
+        std::cout << "file open" << "\n";
+        int n=0; // 0: city 1: country 2: lat 3:long
+        while(getline(file, line)){ // whole file
+            std::stringstream ss(line);
+            std::string lineArray[4];
+            while(getline(ss, line, ',')){ // each line
+                std::cout << n << "\t" << line <<  "\n";
+                lineArray[n] = line;
+                n++;
+                if(n > 3){
+                    n = 0;
+                }
+            }
+            //cities.insert(lineArray[0], lineArray[1], lineArray[2], lineArray[3]);
+        }
+        file.close();
+    } else{
+        std::cout << "Unable to open file";
+    }
+
+    return cities;
+
+}
+
 int main(){
+    readWorldCities("../Projects/Numbers/worldcities_data/worldcities.csv");
 
     return 0;
 }
