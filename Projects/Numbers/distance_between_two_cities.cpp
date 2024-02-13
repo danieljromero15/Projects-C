9356125ae2e9d86c5bf2c0eb09172e654be0f89a
Daniel Romero <danieljromero305@gmail.com>
2024-02-13 14:00:45 -0500

    Worked on distance_between_two_cities.cpp

--- Projects/Numbers/distance_between_two_cities.cpp
+++ Projects/Numbers/distance_between_two_cities.cpp
@@ -13,9 +13,11 @@
 
 struct cityData {
     std::string country;
-    double lat;
-    double lng;
+    double lat{}; // positive is N, negative is S
+    double lng{}; // positive is E, negative is W
 };
+
+std::unordered_map<std::string, cityData> cities_map;
 
 std::unordered_map<std::string, cityData> readWorldCities(const std::string& filePath){
     std::unordered_map<std::string, cityData> cities;
@@ -34,11 +36,13 @@ std::unordered_map<std::string, cityData> readWorldCities(const std::string& fil
                 lineArray[n] = line;
                 n++;
                 if(n > 3){
-                    for(int i = 0; i < 4; i++){
-                        std::cout << lineArray[i] << " ";
-                    }
-                    std::cout << "\n";
-                    n = 0;
+                    cityData tempData;
+                    tempData.country = lineArray[1];
+                    tempData.lat = std::stod(lineArray[2]);
+                    tempData.lng = std::stod(lineArray[3]);
+                    cities[lineArray[0]] = tempData;
+
+                    n = 0; // resets counter
                 }
             }
             //cities.insert(lineArray[0], lineArray[1], lineArray[2], lineArray[3]);
@@ -52,8 +56,41 @@ std::unordered_map<std::string, cityData> readWorldCities(const std::string& fil
 
 }
 
+void printCityData(const std::string& in){
+    std::cout << in << ", ";
+    std::cout << cities_map[in].country << " ";
+
+    double lat = cities_map[in].lat;
+    double lng = cities_map[in].lng;
+
+    std::cout << std::abs(lat);
+    if(lat != std::abs(lat)){ // if lat is S
+        std::cout << "S";
+    }else{ // lat is N
+        std::cout << "N";
+    }
+
+    std::cout << " " << std::abs(lng);
+    if(lng != std::abs(lng)){ // if lng is W
+        std::cout << "W";
+    }else{ // lng is E
+        std::cout << "E";
+    }
+}
+
+
+
 int main(){
-    readWorldCities("../Projects/Numbers/worldcities_data/worldcities.csv");
+    cities_map = readWorldCities("../Projects/Numbers/distance_between_two_cities_data/worldcities.csv");
+
+    std::string input1 = "Tokyo"; // 35.6897N 139.692E
+    std::string input2 = "Atlanta"; // 33.7628N 84.422W
+
+    printCityData(input1);
+    std::cout << "\n";
+    printCityData(input2);
+
+    //double distance = calculateDistance(input1, input2); // should be 11027 or 11035 km
 
     return 0;
 }
