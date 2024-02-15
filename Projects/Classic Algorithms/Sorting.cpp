2d26cdad1eb1915fd576772f955a923e7b92327b
danieljromero15 <danieljromero305@gmail.com>
2024-02-14 19:35:40 -0500

    Worked on Sorting.cpp

--- Projects/Classic Algorithms/Sorting.cpp
+++ Projects/Classic Algorithms/Sorting.cpp
@@ -2,3 +2,26 @@
 // Created by danie on 2/13/2024.
 // Description: Implement two types of sorting algorithms: Merge sort and bubble sort.
 //
+
+#include <iostream>
+#include <vector>
+#include "printCollections.h"
+
+using std::vector;
+
+template<class T>
+T *mergeSort(T array[], int arraySize) {
+    T sortedArray[arraySize];
+    for (int i = 0; i < arraySize / 2; i++) {
+
+    }
+    //std::cout << array[1] << " ";
+    return &sortedArray[0];
+}
+
+template<class T>
+T *copyArray(T arrayToCopy[], int arraySize) {}
+
+int main() {
+    vector<int> array = {1, 2, 3, 4, 5};
+}
