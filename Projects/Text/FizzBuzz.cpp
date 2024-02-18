863ed49dd3d88c86be6400bb7b388d0e961c9d1b
danieljromero15 <danieljromero305@gmail.com>
2024-02-18 16:58:03 -0500

    Completed FizzBuzz.cpp

--- Projects/Text/FizzBuzz.cpp
+++ Projects/Text/FizzBuzz.cpp
@@ -4,3 +4,25 @@
 //      But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 //      For numbers which are multiples of both three and five print “FizzBuzz”.
 //
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        std::string out;
+        if (i % 3 == 0) {
+            out += "Fizz";
+        }
+        if (i % 5 == 0) {
+            out += "Buzz";
+        }
+
+        if (out.empty()) {
+            out += std::to_string(i);
+        }
+
+        std::cout << out << "\n";
+    }
+
+    return 0;
+}
