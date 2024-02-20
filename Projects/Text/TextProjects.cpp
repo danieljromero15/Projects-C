1cbf538e4dbd926830ce171320d945325d488202
Daniel Romero <danieljromero305@gmail.com>
2024-02-20 13:24:30 -0500

    Completed Pig latin

--- Projects/Text/TextProjects.cpp
+++ Projects/Text/TextProjects.cpp
@@ -6,7 +6,7 @@
 #include "text_operations.h"
 
 int main(int argc, char *argv[]) {
-    std::string word = "Bob";
+    std::string word = "Hello";
     if (argc >= 2) {
         // cmdline arguments var definition go here
         word = std::string(argv[1]);
@@ -15,6 +15,8 @@ int main(int argc, char *argv[]) {
     std::cout << "Word is: " << word << std::endl;
 
     std::cout << "Reversed: " << reverseString(word) << std::endl;
+
+    std::cout << "Pig latin: " << pigLatin(word) << std::endl;
 
     //std::cout << word << std::endl;
     return 0;
--- Projects/Text/text_operations.h
+++ Projects/Text/text_operations.h
@@ -17,3 +17,34 @@ std::string reverseString(std::string stringToReverse){
     }
     return reversedString;
 }
+
+//Pig Latin - Pig Latin is a game of alterations played on the English language game.
+// To create the Pig Latin form of an English word the initial consonant sound is transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay).
+// Read Wikipedia for more information on rules.
+
+bool isVowel(char c){
+    const std::string vowels = "aeiouAEIOU";
+    if(vowels.find(c) != std::string::npos){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+std::string pigLatin(std::string word){
+    for(int i = 0; i < word.length(); i++){ // converts whole word to lowercase
+        word[i] = (char)tolower(word[i]);
+    }
+    //std::cout << isVowel(word[0]) << std::endl;
+    if(isVowel(word[0])){
+        word += "way";
+    }else{
+        char temp = word[0];
+        word = word.substr(1, word.length());
+        std::cout << word << std::endl;
+        word += temp;
+        word += "ay";
+    }
+
+    return word;
+}
