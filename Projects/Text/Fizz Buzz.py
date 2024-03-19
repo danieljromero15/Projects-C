bfc68b09148693ff18bce60bc328841ae070eeb5
Daniel Romero <danieljromero305@gmail.com>
2024-03-19 13:55:23 -0400

    Completed Fizz Buzz.py

---
+++ Projects/Text/Fizz Buzz.py
@@ -0,0 +1,15 @@
+# Fizz Buzz - Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead
+# of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five
+# print “FizzBuzz”.
+
+if __name__ == '__main__':
+    num = int(input('Enter a number: '))
+    for i in range(1, num+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 5 == 0:
+            print('Buzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        else:
+            print(str(i))
