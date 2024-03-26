ef1b150621e293c64087e1932221deb392dfbc64
Daniel Romero <danieljromero305@gmail.com>
2024-03-26 11:40:03 -0400

    Created Product Inventory Project

---
+++ Projects/Classes/Product Inventory Project/inventory.cpp
@@ -0,0 +1,13 @@
+//
+// Created by danie on 3/26/2024.
+//
+// Product Inventory Project - Create an application which manages an inventory of products.
+// Create a product class which has a price, id, and quantity on hand.
+// Then create an inventory class which keeps track of various products and can sum up the inventory value.
+
+#include "inventory.h"
+
+int main() {
+
+    return 0;
+}
---
+++ Projects/Classes/Product Inventory Project/inventory.h
@@ -0,0 +1,14 @@
+//
+// Created by danie on 3/26/2024.
+//
+
+#ifndef PROJECTS_INVENTORY_H
+#define PROJECTS_INVENTORY_H
+
+
+class product {
+
+};
+
+
+#endif //PROJECTS_INVENTORY_H
