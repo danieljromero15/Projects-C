ba5d9238c744b4b1bfa8e450ef5ce91c69ff1a45
Daniel Romero <danieljromero305@gmail.com>
2024-03-26 13:07:15 -0400

    Completed Produce Inventory Project

--- Projects/Classes/Product Inventory Project/inventory.cpp
+++ Projects/Classes/Product Inventory Project/inventory.cpp
@@ -5,9 +5,23 @@
 // Create a product class which has a price, id, and quantity on hand.
 // Then create an inventory class which keeps track of various products and can sum up the inventory value.
 
+#include <iostream>
+#include <vector>
 #include "inventory.h"
 
+// It's not 100% functional but that purely comes down to how much I want to mess with random numbers.
+// To be clear, I don't want to mess with random numbers.
 int main() {
+    std::vector<product> inventory;
+
+    inventory.reserve(100);
+    for (int i = 0; i < 100; ++i) {
+        inventory.emplace_back(9.99, i, 10);
+    }
+
+    std::cout << printProduct(inventory, "\n") << std::endl;
+
+    std::cout << "Total inventory: " << productSum(inventory) << std::endl;
 
     return 0;
 }
--- Projects/Classes/Product Inventory Project/inventory.h
+++ Projects/Classes/Product Inventory Project/inventory.h
@@ -7,8 +7,33 @@
 
 
 class product {
+public:
+    float price;
+    int id;
+    int quantity;
 
+    product(float price, int id, int quantity) {
+        this->price = price;
+        this->id = id;
+        this->quantity = quantity;
+    }
 };
 
+
+std::string printProduct(std::vector<product> &vectorToPrint, const std::string &separator){
+    std::string out;
+    for(product &it: vectorToPrint){
+        out += std::to_string(it.price) + ' ' + std::to_string(it.id) + ' ' + std::to_string(it.quantity) + separator;
+    }
+    return out;
+}
+
+int productSum(std::vector<product> &productVector){
+    int sum = 0;
+    for(product &it : productVector){
+        sum += it.quantity;
+    }
+    return sum;
+}
 
 #endif //PROJECTS_INVENTORY_H
