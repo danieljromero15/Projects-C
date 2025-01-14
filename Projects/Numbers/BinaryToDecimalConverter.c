c37e5a900e3164ee418009e7ce0316910aacf32a
Daniel Romero <danieljromero305@gmail.com>
2025-01-14 14:17:18 -0500

    Completed BinaryToDecimalConverter.c with conversion back to decimal

--- Projects/Numbers/BinaryToDecimalConverter.c
+++ Projects/Numbers/BinaryToDecimalConverter.c
@@ -4,31 +4,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 const int MAX_BINARY_LENGTH = 50;
 
-int strLength(const char* strToFindLength){
+/*int strLength(const char* strToFindLength){
     int length = 0;
     while(strToFindLength[length] != '\0'){
         length++;
     }
     return length;
-}
+}*/
 
 void reverseStr(char* strToReverse){
-
-    int length = strLength(strToReverse);
+    int length = strlen(strToReverse); // error is here, length is not correct
+    //printf("len of str %d", length);
     for(int i = 0; i < (length / 2); i++){
         char last = strToReverse[length - i - 1];
         char first = strToReverse[i];
         strToReverse[i] = last;
         strToReverse[length - i - 1] = first;
     }
-    //printf("\n%s", strToReverse);
+    //printf("\nstrToReverse: %s\n", strToReverse);
 }
 
 void toBinary(unsigned long numToConvert, char *numStr){
-    printf("\n%ld %s", numToConvert, numStr);
+    //printf("\n%ld %s", numToConvert, numStr);
     int numStrIndex = 0;
     while(numToConvert > 0){
         if (numToConvert % 2 != 0){
@@ -43,6 +45,32 @@ void toBinary(unsigned long numToConvert, char *numStr){
     reverseStr(numStr);
 }
 
+void printStats(char* str){
+    printf("\n%s, sizeof %llu\n", str, strlen(str));
+}
+
+unsigned long toDecimal(char *numStr){
+
+    unsigned long long length = strlen(numStr);
+    unsigned long result = 0;
+    char numStrReversed[length];
+    //char *endptr;
+
+    //printf("\n%s, length %d, sizeof %llu\n", numStr, length, sizeof(numStr));
+    //printStats(numStr);
+    //memcpy(numStrReversed, numStr, length);
+    strcpy(numStrReversed, numStr); // omg the solution was using strcpy instead of memcpy aaaaaaaaa
+    //printStats(numStrReversed);
+    reverseStr(numStrReversed);
+    //printf("%s\t%s\n", numStr, numStrReversed);
+    for(int i=0; i < length;i++){
+        if(numStrReversed[i] == '1'){
+            result += (unsigned long) pow(2, i);
+        }
+    }
+    return result;
+}
+
 int main(){
     unsigned long numToConvert;
     char binaryVal[MAX_BINARY_LENGTH];
@@ -54,4 +82,8 @@ int main(){
     printf("\nConverting %ld to binary", numToConvert);
     toBinary(numToConvert, (char *) &binaryVal);
     printf("\n%ld in binary is %s", numToConvert, binaryVal);
+
+    unsigned long decimal = toDecimal(binaryVal);
+    //printf("\nFinal result: %lu", decimal);
+    printf("\nConverting %s back to decimal is %lu\n", binaryVal, decimal);
 }
